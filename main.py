71278e63dc1fc9e7618ba9894215023ef915786d
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2017-11-15 00:25:59 +0700

    added segment image for gray background

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import cv2
 
 img = Image.open("asset/101_1.tif") #load image from file
 # segmentedImg = segment_image(img,25,85) #best threshold and size so far
-segmentedImg = segment_image(img,15,55) #threshold lv 1
-segmentedImg2 = segment_image(segmentedImg,15,60) #threshold lv 2
-enhanced_image = Image.fromarray(image_enhance(numpy.asarray(segmentedImg2)))
+# segmentedImg = segment_image(img,20,55) #threshold lv 1
+# segmentedImg2 = segment_image(segmentedImg,15,60) #threshold lv 2
+segmentedImg = segment_image(img,30,100) #for dataset with gray background
+segmentedImg.show()
+enhanced_image = Image.fromarray(image_enhance(numpy.asarray(segmentedImg)))
 enhanced_image.show()
