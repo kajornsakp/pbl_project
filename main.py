57024c725345cf425f88ddbd6dc8f5fcfb25496c
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2017-11-14 11:27:34 +0700

    no message

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from segmentator.segmentation import segment_image
 from PIL import Image
 
 
-img = Image("Image Path") #load image from file
+img = Image.open("Image path") #load image from file
 segmentedImg = segment_image(img,25,85) #best threshold and size so far
+segmentedImg.show()
